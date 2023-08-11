5ec5c84dd0a28e2d3f44434ed4e7392154469da0
刘杰文 <1635370493@qq.com>
2023-08-11 21:05:13 +0800

    Update main_lv2.js

--- assets/js/main_lv2.js
+++ assets/js/main_lv2.js
@@ -1,7 +1,9 @@
 var sectionHeight_lv2 = function() {
+    console.log('Triggered: main_lv2.js')
     if ( $(section).attr('name') = 'star_section' ) {
         $('section').css('height','min-content');
     } else {
+        console.log('Triggered: main_lv2.js --> main.js')
         sectionHeight()
     }
 }
