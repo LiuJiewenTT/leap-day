ef0930e0851a442091493607f8a1c72be68bc853
刘杰文 <1635370493@qq.com>
2023-08-11 21:01:42 +0800

    Create main_lv2.js

---
+++ assets/js/main_lv2.js
@@ -0,0 +1,9 @@
+var sectionHeight_lv2 = function() {
+    if ( $(section).attr('name') = 'star_section' ) {
+        $('section').css('height','min-content');
+    } else {
+        sectionHeight()
+    }
+}
+
+$(window).resize(sectionHeight_lv2);
